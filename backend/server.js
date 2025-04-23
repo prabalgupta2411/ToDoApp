110e03b3ce45a17c233319eb886322de4f24e27b
prabalgupta2411 <er.prabbalgupta.2411@gmail.com>
2025-04-23 13:16:42 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,6 @@ app.use(express.json());
 
 // Add headers middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
